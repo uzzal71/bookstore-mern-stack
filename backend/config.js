b0a2e142fcdc56968f8ffb51a3c58467bb0cbc0b
uzzal71 <uzzalroy.acm@gmail.com>
2024-08-06 22:35:35 +0600

    database connection

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb://127.0.0.1:27017/bookstores?retryWrites=true&w=majority';
+// export const mongoDBURL = 'mongodb://127.0.0.1:27017/bookstores?retryWrites=true&w=majority';
+export const mongoDBURL = 'mongodb+srv://uzzalroy71:nli2NbNR6dUjyiGL@cluster0.ysa2z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
-import { PORT } from "./config.js";
+import { mongoDBURL, PORT } from "./config.js";
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get("/", (request, response) => {
+    return response.status(234).send('Welcome to MERN Stack Tutorial');
 });
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('Connected to MongoDB...');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
