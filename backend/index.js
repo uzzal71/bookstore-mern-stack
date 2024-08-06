6daf93cb4e6f41abfd2b41c41a5b4ca10e1f6161
uzzal71 <uzzalroy.acm@gmail.com>
2024-08-06 22:58:02 +0600

    Backend API Complete

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
 import express from 'express';
-import { mongoDBURL, PORT } from "./config.js";
+import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+app.use(cors());
 
 app.get("/", (request, response) => {
     return response.status(234).send('Welcome to MERN Stack Tutorial');
 });
+
+app.use('/books', booksRoute);
 
 mongoose
     .connect(mongoDBURL)
